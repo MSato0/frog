b7aa0c935241a6d8730dc1743ba724ac2a5e89d0
MSato0 <mayu-satou@tas-japan.comm>
2020-08-21 17:26:36 +0900

    pylintのコーディング規約に従う

--- sato/pseudonyms.py
+++ sato/pseudonyms.py
@@ -1,21 +1,28 @@
-import sys, random
+"""project 1 : Funny Name Generator"""
+import sys
+import random
 
-print("welcome Name Picker \n")
-print("A name just like sean world pick for Gus\n")
+def main():
+    """２つのタプルからランダムに名前を選んで画面に表示する"""
+    print("welcome Name Picker \n")
+    print("A name just like sean world pick for Gus\n")
 
-first = ('sato', 'kato', 'ito', 'kudo')
-last = ('hanako', 'taro', 'ichiro', 'sestuko')
+    first = ('sato', 'kato', 'ito', 'kudo')
+    last = ('hanako', 'taro', 'ichiro', 'sestuko')
 
-while True:
-    firstName = random.choice(first)
-    lastName  = random.choice(last)
+    while True:
+        first_name = random.choice(first)
+        last_name = random.choice(last)
 
-    print("\n\n")
-    print("{} {}".format(firstName, lastName), file=sys.stderr)
-    print("\n\n")
+        print("\n\n")
+        print("{} {}".format(first_name, last_name), file=sys.stderr)
+        print("\n\n")
 
-    try_again = input("\n\nTry again? (Press Enter else n to quit)\n")
-    if try_again.lower() == "n" :
-        break
+        try_again = input("\n\nTry again? (Press Enter else n to quit)\n")
+        if try_again.lower() == "n":
+            break
 
-input("\nPress Enter to exit")
+    input("\nPress Enter to exit")
+
+if __name__ == "__main__":
+    main()
